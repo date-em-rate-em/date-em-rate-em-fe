2d9c2a2717236aeedaedb7a85a69450c58278a90
mendozalourdes <lourd.mendoza@gmail.com>
2021-08-26 19:30:51 -0600

    Add imports and structure for ReviewForm.

---
+++ src/Components/ReviewForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ReviewForm extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    componentDidMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+ReviewForm.propTypes = {
+
+};
+
+export default ReviewForm;
