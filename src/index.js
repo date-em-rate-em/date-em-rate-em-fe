ff2bbee1872c5e413e5b748dd9a6d98dd310ae82
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-27 09:34:32 -0400

    Add MenuItems component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
-import App from './App';
+import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
