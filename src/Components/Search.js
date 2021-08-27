07a4a61b0296af0b62049dc96690de6d9aef7f35
mendozalourdes <lourd.mendoza@gmail.com>
2021-08-26 19:31:12 -0600

    Add imports and structure for Search.

---
+++ src/Components/Search.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+Search.propTypes = {
+
+};
+
+export default Search;
