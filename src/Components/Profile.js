e10fc4ac6e48884e36a6409e6f2864330bc76eea
mendozalourdes <lourd.mendoza@gmail.com>
2021-08-26 19:24:27 -0600

    Add imports and structure for Profile.

---
+++ src/Components/Profile.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    componentDidMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+Profile.propTypes = {
+
+};
+
+export default Profile;
