85fe7fbcb3e69454f81e9ec81250d7f1cb707e9d
mendozalourdes <lourd.mendoza@gmail.com>
2021-08-28 12:58:53 -0600

    Add section where reviews will populate.

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Route } from "react-router-dom";
 import { useState, useEffect } from 'react';
+import Review from './Review';
 // import Review from './Review';
 
 const Profile = () => {
     return (
         <div>
+            <h1>Client Profile</h1>
             <section className="client-contact-info">
                 <h1>Client Contact Information</h1>
                     <p>(this is where the client's phone number/email/contact information will go from state)</p>
             </section>
             <section className="add-review-btn-section">
             <button className="add-review-btn">
-                        Add Review
+                        Add New Review
                 </button>
+            </section>
+            <section>
+                {/* <Review/> */}
             </section>
         </div>
     );
