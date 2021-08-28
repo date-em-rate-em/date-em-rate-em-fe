ffe88a4f5285a6d6873e372a4ee2b6fadde94082
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-28 16:03:53 -0600

    Update return on Dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -3,14 +3,15 @@ import { useState } from 'react';
 export const Dashboard = (userName, clients) => {
     const [ highClientCard, setHigh ] = useState({});
     const [ lowClientCard, setLow ] = useState({});
+    const [ featured, setclientCard ] = useState({});
 //if we want to try to find high/low client...
 // const sortedClients = clients.sort((a, b) => a.avgRating - b.avgRating)
 // const topClient = sortedClients[0]
 // setHigh(topClient);
 
-//otherwise, we could just do a random client of the day...
-// const randomClient = clients[Math.floor(Math.random() * clients.length)];  
-// setClientCard(randomClient); 
+//otherwise, we could just do a featured client of the day...
+// const featuredClient = clients[Math.floor(Math.random() * clients.length)];  
+// setClientCard(featuredClient); 
 
     return (
         <section>
@@ -22,13 +23,16 @@ export const Dashboard = (userName, clients) => {
                     client={highClientCard}
                     />
                 </Link>
-                <h3>Avoid this one:</h3>
+                <h3>Here's one to avoid:</h3>
                 <Link to='/Profile'>
                     <ContactCard 
                     client={lowClientCard}
                     />
                 </Link>
             </article>
+            <section className='resource'>
+                {/* Resource?? */}
+            </section>
         </section>
     );
 };
