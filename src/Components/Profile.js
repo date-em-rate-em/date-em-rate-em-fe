2e0c00f1da96f2c18c8e3d777718358c654ee5d9
mendozalourdes <lourd.mendoza@gmail.com>
2021-08-28 12:50:50 -0600

    Add section for client contact info to profile.

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,30 +1,20 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Route } from "react-router-dom";
+import { useState, useEffect } from 'react';
+// import Review from './Review';
 
+const Profile = () => {
+    return (
+        <div>
+            <section className="client-contact-info">
+                <h1>Client Contact Information</h1>
+                    <p>(this is where the client's phone number/email/contact information will go from state)</p>
+            </section>
+            <section className="add-review-btn-section">
 
-class Profile extends Component {
-    constructor(props) {
-        super(props);
-
-    }
-
-
-    componentDidMount() {
-
-    }
-
-
-    render() {
-        return (
-            <div>
-
-            </div>
-        );
-    }
-}
-
-Profile.propTypes = {
-
+            </section>
+        </div>
+    );
 };
 
 export default Profile;
