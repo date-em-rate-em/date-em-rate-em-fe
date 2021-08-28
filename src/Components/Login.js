a3667525fb384a61669400bfba9a7807b5deb28c
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-28 15:29:27 -0600

    Update Login Component structure and return

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
+import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-class Login extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+export const Login = () => {   
+
+    const [ user, setUser ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    return (
+        <section className='login'>
+            <h1>ClientTell</h1>
+            <article className='login-content'>
+                <p>Account Login</p>
+                <form className='login-credentials'>
+                    <input 
+                    type='text'
+                    name='username' 
+                    autocomplete='off' 
+                    placeholder='Username'
+                    required/>
+                    <label for='password-input'>Password:</label>
+                    <input 
+                    type='password' 
+                    autocomplete='off'
+                    name='password'
+                    placeholder='Password' 
+                    required/>
+                    <button className='loginBtn'>Log in</button>
+                    {/* <p class='login-error hidden' id='loginError'>Login Error. Please try again.</p> */}
+                </form>
+            </article>
+        </section>
+    ); 
+
 }
-
-export default Login;
