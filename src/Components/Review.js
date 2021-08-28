ede9ea627ddac4bad08251ac84a91d6409b51c2d
mendozalourdes <lourd.mendoza@gmail.com>
2021-08-28 13:51:35 -0600

    Include review information to review card.

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Review = () => {
+    // const Review = ({id, shortReview, title, rating, tagline, size, payment, extendedReview, kindness, vibe }) => {
+
     return (
-        <div>
-            
+        <div className="each-review">
+ <h2 className="review-title">This Client....(title)</h2>
+            <p className="short-review">(short review goes here)</p>
+            <p className="extended-review">(Review Goes Here)</p>
+            <p className="rating">(rating goes here) </p>          
         </div>
     );
 };
