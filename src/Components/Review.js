2d96d056c1cbd1f3535bba0c7d44488f05ccdf19
mendozalourdes <lourd.mendoza@gmail.com>
2021-08-28 13:55:27 -0600

    Include rest of review data to review card.

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -8,7 +8,11 @@ const Review = () => {
  <h2 className="review-title">This Client....(title)</h2>
             <p className="short-review">(short review goes here)</p>
             <p className="extended-review">(Review Goes Here)</p>
-            <p className="rating">(rating goes here) </p>          
+            <p className="rating">(rating goes here) </p>       
+            <p className="size-data">(size info goes here)</p>   
+            <p className="payment-data">(payment info goes here)</p>            
+            <p className="kindness-meter">(kindness meter goes here)</p>            
+            <p className="vibe-data">(vibe info goes here)</p>     
         </div>
     );
 };
