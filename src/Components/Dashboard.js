8301bffeb9244e21a3df70834b4454f92a818ba7
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-28 16:00:50 -0600

    Build out return on Dashboard for featured clients

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
 
-const Dashboard = () => {
+export const Dashboard = (userName, clients) => {
+    const [ highClientCard, setHigh ] = useState({});
+    const [ lowClientCard, setLow ] = useState({});
+//if we want to try to find high/low client...
+// const sortedClients = clients.sort((a, b) => a.avgRating - b.avgRating)
+// const topClient = sortedClients[0]
+// setHigh(topClient);
+
+//otherwise, we could just do a random client of the day...
+// const randomClient = clients[Math.floor(Math.random() * clients.length)];  
+// setClientCard(randomClient); 
+
     return (
-        <div>
-            
-        </div>
+        <section>
+            <h2>Welcome back, {userName}!</h2>
+            <article>
+                <h3>Today's Top Client:</h3>
+                <Link to='/Profile'>
+                    <ContactCard 
+                    client={highClientCard}
+                    />
+                </Link>
+                <h3>Avoid this one:</h3>
+                <Link to='/Profile'>
+                    <ContactCard 
+                    client={lowClientCard}
+                    />
+                </Link>
+            </article>
+        </section>
     );
 };
-
-export default Dashboard;
