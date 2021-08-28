068cc2f28b0e6032f77f9f080794125679eae37b
mendozalourdes <lourd.mendoza@gmail.com>
2021-08-28 17:06:52 -0600

    Add reviewCard info to component.

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import ReviewCard from './ReviewCard';
 
 const Review = () => {
+
+    const allReviews = allReviews.map(review => {
+        return (
+            <ReviewCard
+                id={review.id}
+                key={review.id}
+                name={review.name}
+                amount={review.amount}
+            />
+        )
+    })
+
     return (
         <div>
-            
+            {/* {allReviews} */}
         </div>
     );
 };
