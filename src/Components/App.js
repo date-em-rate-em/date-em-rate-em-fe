27a8c1aab8bb3e43b73f07b0113d5280037cbacf
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-30 13:53:54 -0600

    Test Footer icons appearing

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,17 @@
 // import logo from './logo.svg';
 import { Login } from './Login';
+import { Footer } from './Footer';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faSearch, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faPlus, faHome } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
 
-  library.add(fab, faSearch, faPlus)
+  library.add(fab, faSearch, faPlus, faHome)
 
   return (
     <div className="App">
-      <Login />
+      <Footer />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
