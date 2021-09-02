acfdb370028b08e7d178a8d54a938a18b615e269
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-02 17:53:29 -0600

    Adjust error handling for dashboard.

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
 import React from 'react';
-import ContactCard from './ContactCard';
+import { ContactCard } from './ContactCard';
 
 export const Dashboard = ({ user, clients }) => {
     const [ highClientCard, setHigh ] = useState({});
     const [ lowClientCard, setLow ] = useState({});
     const [ featuredClient, setClientCard ] = useState({});
 
-    // console.log('DASHBOARD USER', user)
-    // console.log('DASHBOARD CLIENTS', clients)
+    console.log('DASHBOARD USER', user)
+    console.log('DASHBOARD CLIENTS', clients)
 
 
 // otherwise, we could just do a featured client of the day...
@@ -18,7 +18,7 @@ const randomClient = clients[Math.floor(Math.random() * clients.length)];
 useEffect(() => {
     if (randomClient) {
         setClientCard(randomClient);
-        // console.log("feature", featuredClient.email)
+        console.log("feature", featuredClient)
     }
 
     if(clients) {
@@ -37,43 +37,40 @@ const sortByRatings = (clients) => {
     return sortAverages
   }
 
+  console.log("feature222", featuredClient)
 
     return (
         <section>
             <h2>Welcome Back to ClienTell</h2>
             <article>
-                {/* <h3>Today's Featured Client:</h3> */}
-                {/* {clients && featuredClient && <ContactCard highlyRated={highClientCard} lowRated={lowClientCard} featuredClient={featuredClient}/>} */}
-                {/* // {featuredClient.averageRating} */}
-                {/* // {featuredClient.reviews[0].body} */}
-                {/* {featuredClient.reviews[1]} */}
-                {clients && featuredClient &&
+              
+                {clients.length && featuredClient.id &&
                     <ContactCard 
                     id={featuredClient.id}
                     email={featuredClient.email}
                     averageRating={featuredClient.averageRating}
-                    reviews={featuredClient.reviews}
+                    reviews={featuredClient.reviews.length}
                     // client={highClientCard}
                     />
 
                 }
-                {clients && highClientCard &&
+                {clients.length && highClientCard.id &&
                     <ContactCard 
                     id={highClientCard.id}
                     email={highClientCard.email}
                     averageRating={highClientCard.averageRating}
-                    reviews={highClientCard.reviews}
+                    reviews={highClientCard.reviews.length}
                     // client={highClientCard}
                     />
 
                 }
                 <h3>Here's one to avoid:</h3>
-                {clients && lowClientCard && 
+                {clients.length && lowClientCard.id && 
                     <ContactCard 
                      id={lowClientCard.id}
                     email={lowClientCard.email}
                   averageRating={lowClientCard.averageRating}
-                  reviews={lowClientCard.reviews}
+                  reviews={lowClientCard.reviews.length}
 
                     />
                 }
