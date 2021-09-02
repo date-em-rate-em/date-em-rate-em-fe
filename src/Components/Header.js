ddaad7d3e7293fbd2959c5895c322882e6c7f122
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-02 18:51:34 -0400

    Add Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+export const Header = () => {
+
+    const exitURL = 'https://weather.com/'
     return (
-        <div>
-            
-        </div>
+        <section>
+            <div className='header-left'>
+                <Link to={exitURL}>
+                    <button>Quick Exit</button>
+                </Link>
+                <h1>ClienTell</h1>
+            </div>
+            <div className='header-links'>
+                <Link to='/'>
+                    <button>Home</button>
+                </Link>
+                <Link to='/Search'>
+                    <button>Search Clients</button>
+                </Link>
+                <Link to='/AddClient'>
+                    <button>Add Client</button>
+                </Link>
+                <Link to='/Resources'>
+                    <button>Resources</button>
+                </Link>
+            </div>
+        </section>
     );
 };
-
-export default Header;
