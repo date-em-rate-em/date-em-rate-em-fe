b01474a0a87cf1216f03f31d926f6dd9ec552384
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-02 18:57:51 -0400

    merge conflicts

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -8,31 +8,37 @@ export const Search = ({ clients }) => {
    const [ searchTerm, setSearchTerm ] = useState('');
 
    //we will need to do some error handling with the way someone types in the phone number/email...
+//    console.log('ALL CLIENTS', allClients)
    const clientCard = allClients.filter((client) => {
-      if (client.email.toLowerCase().includes(searchTerm.toLowerCase())) {
-           return (
-               <ContactCard 
-               id={client.id}
-               key={client.id}
-               email={client.emailAddress}
-               averageRating={client.averageRating}
-              />
-           )
-       }
-   });
+      if (searchTerm === '') {
+          return client
+      } else if (client.email.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return client 
+      }
+   }).map(client => {
+     return (
+        <ContactCard 
+        id={client.id}
+        key={client.id}
+        email={client.email}
+        averageRating={client.averageRating}
+        reviews={client.reviews}
+       />
+    )
+   })
 
    return (
-    <section className='searchView'>
+    <section className='search-view'>
         <input className='search-bar'
             type='text' 
-            placeholder='Search by client email...'
+            placeholder='Search for client by email...'
             onChange={(event) => {
                 setSearchTerm(event.target.value);
             }} 
         />
-        <article className='cards-container'>
+        <div className='cards-container'>
         {clientCard.length ? clientCard : <h2> There are no clients that match your search.</h2>} 
-        </article>
+        </div>
     </section>
 )
 }
