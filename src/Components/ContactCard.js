c909ef036180aceacd2946c80bf399b2d7736eba
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-02 15:46:07 -0600

    Create contact cards for featured, top, and low rated clients.

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const ContactCard = () => {
+const ContactCard = ({featuredClient, lowRated, highlyRated}) => {
+
     return (
-        <div>
-            
-        </div>
+        <section className="contact-card-section">
+            <div className="featured-client-contact-card">
+                <h2>Client's Data</h2>
+                <p className="email">Email: {featuredClient.email} </p>
+                <p className="average-rating">Average Rating: {featuredClient.averageRating} </p>
+            </div>
+            <div className="high-rated-client-card">
+                <h2>Highly Rated Client's Data</h2>
+                <p className="email">Email: {highlyRated.email} </p>
+                <p className="average-rating">Average Rating: {highlyRated.averageRating} </p>
+            </div>
+            <div className="low-rated-client-card">
+                <h2>Low Rated Client's Data</h2>
+                <p className="email">Email: {lowRated.email} </p>
+                <p className="average-rating">Average Rating: {lowRated.averageRating} </p>
+            </div>
+
+        </section>
     );
 };
 
