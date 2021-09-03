b2240b2262cc8d6f6b6f6f90d34c3bbf92e5d181
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-02 18:52:36 -0600

    Add routes to components

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@ import { Login } from './Login';
 import { Header } from './Header';
 import { Search } from './Search';
 import { Dashboard } from './Dashboard';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faSearch, faPlus, faHome } from '@fortawesome/free-solid-svg-icons'
@@ -10,6 +10,11 @@ import { CLIENT_DATA_QUERY } from '../index'
 import { USER_DATA_QUERY } from '../index'
 import { useQuery } from '@apollo/client';
 import { useState, useEffect } from 'react';
+import Header from './Header'
+import Profile from './Profile'
+import ReviewForm from './ReviewForm'
+import ReviewFormDetails from './ReviewFormDetails';
+// import { Search } from 'history';
 
 const App = () => {
   library.add(fab, faSearch, faPlus, faHome)
@@ -27,18 +32,51 @@ const App = () => {
           // console.log("clients", clients)
         }
       }, [clients, clientsData, clientsLoading, userData, userLoading, user]);
-
-  return (
-    <div className="App">
-      {clients.length && user && 
-      <Dashboard 
-      clients={clients}
-      user={user}
-      />}
-      {/* <Footer />
-     */}
-    </div>
+   
+   return (
+    <main>
+      <div className="App">
+        <Header />
+        {userLoading && clientsLoading && <p>Loading...</p> }
+        <Switch>
+          <Route exact path="/" render={() => 
+            <Dashboard 
+            clients={clients} 
+            user={user} 
+            />
+            }
+          />
+      {/* <Route exact path="/Search" render={() => 
+        <Search clients={clients} user={user} />
+            }
+          /> */}
+      <Route exact path="/profile/:id" render={({match}) => 
+            <Profile 
+            clients={clients} 
+            user={user} 
+            />
+            }
+          />
+      <Route exact path="/review-form-page-1" render={() => 
+            <ReviewForm 
+            clients={clients} 
+            user={user} 
+            />
+            }
+          />
+       <Route exact path="/review-form-page-2" render={() => 
+            <ReviewFormDetails 
+            clients={clients} 
+            user={user} 
+            />
+            }
+          />
+        </Switch>
+        <Footer/>
+      </div>
+    </main>
   );
 }
+
 
 export default App;
