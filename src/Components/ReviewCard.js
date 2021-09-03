7dc2ee4001612ecaa08ff253d4060ee8fdb33146
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-02 20:53:09 -0600

    Adjust Review card to add labels and future data points.

--- src/Components/ReviewCard.js
+++ src/Components/ReviewCard.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
-const ReviewCard = () => {
-    // const Review = ({id, date, review, title, rating, size, payment, kindness, vibe, gender, wouldIGoOutWithThemAgain, safety }) => {
+// const ReviewCard = () => {
+    const ReviewCard = ({id, key, date, body, title, rating, size, payment, kindness, vibe, gender, dateAgain, safetyMeter }) => {
 
     return (
         <div className="each-review">
- <h2 className="review-title">This Client....(title)</h2>
-            <p className="short-review">(short review goes here)</p>
-            <p className="extended-review">(Review Goes Here)</p>
-            <p className="date">(date goes here) </p>   
-            <p className="rating">(rating goes here) </p>       
-            <p className="size-data">(size info goes here)</p>   
-            <p className="payment-data">(payment info goes here)</p>            
-            <p className="kindness-meter">(kindness meter goes here)</p>            
-            <p className="vibe-data">(vibe info goes here)</p>   
-            <p className="date-again">(Would i date again)</p>   
-            <p className="safety-meter">(how safe did i feel)</p>   
-            <p className="gender">(gender)</p>   
+        <h2 className="review-title">Client's Review from the date on {date}:</h2>
+            <p className="body">Review From This Date: {body}</p>
+            <p className="safety-meter">How Safe Did I Feel: {safetyMeter}</p>   
+            <p className="rating">Rating: {rating} </p>       
+            <p className="size-data">Size: {size}</p>   
+            <p className="payment-data">How Well Did They Pay: {payment}</p>            
+            <p className="kindness-meter">Kindness: {kindness}</p>            
+            <p className="vibe-data">Vibe: {vibe}</p>   
+            <p className="date-again">Would I Date Them Again? {dateAgain}</p>   
+            <p className="gender">Gender: {gender}</p>  
+            {/* <p className="date">{date}</p>    */}
+            {/* <p className="vibe-data">Hygiene: {hygiene}</p>   
+            <p className="date-again">How Punctual Were They? {punctuality}</p>   
+            <p className="gender">Condom Usage: {condomUsage}</p>   
+            <p className="date-again">How Long Did the Date Last? {dateDuration}</p>   
+            <p className="gender">Kinks: {kinks}</p>    */}
         </div>
     );
 };
