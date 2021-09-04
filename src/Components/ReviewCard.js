b5028d82a5ed12fe1f185377f5262ccc7899a16b
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-04 16:14:18 -0400

    update reviewCard

--- src/Components/ReviewCard.js
+++ src/Components/ReviewCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { starRating } from '../utils/util';
+import { starRating, formatPayment, formatSize } from '../utils/util';
 
 // const ReviewCard = () => {
     const ReviewCard = ({id, key, date, body, title, rating, size, payment, kindness, vibe, gender, dateAgain, safetyMeter }) => {
@@ -10,12 +10,12 @@ import { starRating } from '../utils/util';
             <p className="body">"{body}"</p>
             <p className="safety-meter">How Safe Did I Feel: {safetyMeter}</p>   
             <p className="rating">Rating: {starRating(rating)} </p>       
-            <p className="size-data">Size: {size}</p>   
-            <p className="payment-data">How Well Did They Pay: {payment}</p>            
+            <p className="size-data">Size: {formatSize(size)}</p>   
+            <p className="payment-data">How Well Did They Pay: {formatPayment(payment)}</p>            
             <p className="kindness-meter">Kindness: {kindness}/10</p>            
             <p className="vibe-data">Vibe: {vibe}/10</p>   
-            <p className="date-again">Would I Date Them Again? {dateAgain}</p>   
-            <p className="gender">Gender: {gender}</p>  
+            <p className="date-again">Would I Date Them Again? {dateAgain}</p> 
+            {gender && <p className="gender">Gender: {gender}</p>}  
             {/* <p className="date">{date}</p>    */}
             {/* <p className="vibe-data">Hygiene: {hygiene}</p>   
             <p className="date-again">How Punctual Were They? {punctuality}</p>   
