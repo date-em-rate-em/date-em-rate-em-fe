dbbc359903865db1da79ef462cb9751569c102ff
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-04 15:05:23 -0600

    Added a few tests to dashboard.

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -15,7 +15,23 @@ describe('Dashboard View', () => {
     });
   
     it('When the page is visited, you should see a header', () => {
-      cy.get('h2').contains("ClienTell")
+      cy.get('h1').contains("ClienTell")
     });
+
+    it('When the page is visited, you should see a welcome message', () => {
+        cy.get('h2').contains("Welcome")
+      });
+      
+   it('When the page is visited, you should see a featured client', () => {
+        cy.get('h3').contains("Featured")
+      });
+
+   it('When the page is visited, you should see a highly rated client', () => {
+        cy.get('h3').contains("Highly")
+      });
+
+   it('When the page is visited, you should see a low rated client', () => {
+        cy.get('h3').contains("Low")
+      });
   
   });
