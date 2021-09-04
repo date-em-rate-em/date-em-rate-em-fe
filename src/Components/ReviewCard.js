6501b407f5f1a753ab942d220fde366812ed7f88
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-04 15:52:22 -0400

    Update reviewcard styling

--- src/Components/ReviewCard.js
+++ src/Components/ReviewCard.js
@@ -5,15 +5,15 @@ import { starRating } from '../utils/util';
     const ReviewCard = ({id, key, date, body, title, rating, size, payment, kindness, vibe, gender, dateAgain, safetyMeter }) => {
 
     return (
-        <div className="each-review">
+        <div className="each-review" id={id}>
         <h2 className="review-title">Client's Review from the date on {date}:</h2>
-            <p className="body">Review From This Date: {body}</p>
+            <p className="body">"{body}"</p>
             <p className="safety-meter">How Safe Did I Feel: {safetyMeter}</p>   
             <p className="rating">Rating: {starRating(rating)} </p>       
             <p className="size-data">Size: {size}</p>   
             <p className="payment-data">How Well Did They Pay: {payment}</p>            
-            <p className="kindness-meter">Kindness: {kindness}</p>            
-            <p className="vibe-data">Vibe: {vibe}</p>   
+            <p className="kindness-meter">Kindness: {kindness}/10</p>            
+            <p className="vibe-data">Vibe: {vibe}/10</p>   
             <p className="date-again">Would I Date Them Again? {dateAgain}</p>   
             <p className="gender">Gender: {gender}</p>  
             {/* <p className="date">{date}</p>    */}
