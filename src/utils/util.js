5e673b3110f4a9775d290353615feccc190d1400
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-04 16:15:44 -0400

    add helper func in util file

--- src/utils/util.js
+++ src/utils/util.js
@@ -2,3 +2,18 @@ export const starRating = (averageRating) => {
     const stars = '⭐️'
     return stars.repeat(Math.floor(averageRating))
   }
+
+export const formatPayment = (payment) => {
+    const dolla = '💲'
+    return dolla.repeat(Math.floor(payment))
+}
+
+export const formatSize = (size) => {
+    if(size <= 3) {
+        return 'Teeny weeny.'
+    } else if(size >= 4 && size <=6){
+        return 'Meh, nothing to write home about.'
+    } else {
+        return 'Above average.'
+    }
+}
