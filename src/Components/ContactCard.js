20577578c6f01d548bd2b0784438d81befeb7af3
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-04 16:14:07 -0400

    import email icon in contact card

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { starRating } from '../utils/util';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const ContactCard = ({ id, key, email, averageRating, reviews }) => {
 
     return (
         <NavLink to={`/profile/${id}`}>
             <article className='contact-card' id={id}>
+                <button className='email'>
+                    <FontAwesomeIcon icon='envelope-square'/>
+                </button>
                 <h3>{email}</h3>
                 <p>Average Rating: {starRating(averageRating)}</p>
                 <p># of reviews: {reviews.length}</p>
