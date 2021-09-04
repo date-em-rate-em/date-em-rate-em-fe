d480a54a90b7796ba43327895e078b39a09d66a8
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-04 14:49:35 -0400

    Add temporary Resource page

---
+++ src/Components/Resources.js
@@ -0,0 +1,20 @@
+
+export const Resources = () => {
+
+    return (
+        <section>
+            <h2 className='under-construction'>Page currently under construction</h2>
+            <p>In the meantime, please click on the links below for resources...</p>
+            <a target="_blank" href='https://sexworkersproject.org/resources/'>
+                    <button className='swp'>Sex Workers Project</button>
+            </a>
+            <a target="_blank" href='https://www.nswp.org/resources'>
+                    <button className='nswp'>The Global Network of Sex Work Projects(NSWP)</button>
+            </a>
+            <a target="_blank" href=' https://swp.urbanjustice.org/news-room/'>
+                    <button className='nswp'>Urban Justice Center</button>
+            </a>
+        </section>
+
+    )
+}
