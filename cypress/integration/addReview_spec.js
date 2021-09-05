4a1c1249e57dbe2e574b154f65e0eac508d71f35
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-05 13:21:02 -0600

    Adjust tests for mutation.

--- cypress/integration/addReview_spec.js
+++ cypress/integration/addReview_spec.js
@@ -1,5 +1,43 @@
 import { aliasClientsQuery, aliasMutation } from "../utils/graphql-test-utils";
 
+const newClientReviewMutation = `
+mutation {
+  allClients: [
+    {
+    "id": "1",
+    "email": "firstClient@email.com",
+    "createdAt": "2021-09-02T22:45:53Z",
+    "updatedAt": "2021-09-02T22:45:53Z",
+    "averageRating": 5,
+    "averageSafetyMeter": 7.0,
+    "averageSize": 3.0,
+    "averageHygiene": null,
+    "averageDuration": null,
+    "reviews": [
+    {
+    "body": "This was a fine date.",
+    "clientId": 14,
+    "condoms": "Was very willing to use a condom.",
+    "createdAt": "2021-09-05T02:40:24Z",
+    "dateAgain": "Yes",
+    "duration": 2,
+    "gender": "Didn't Ask",
+    "hygiene": 2,
+    "id": "40",
+    "payment": 2,
+    "punctuality": "Showed up a little early, but waited.",
+    "rating": 3,
+    "safetyMeter": 10,
+    "size": 2,
+    "updatedAt": "2021-09-05T02:40:24Z",
+    "userId": 1,
+    "vibe": 2
+    }
+  ]
+  }
+  ]
+}`
+
 describe('Add New Review View', () => {
     beforeEach(() => {
       cy.intercept(
@@ -24,6 +62,8 @@ describe('Add New Review View', () => {
          .location("pathname")
          .should("eq", "/add-review/1")
          cy.get('.form-heading')
+
+      
     });
   
     it('When the page is visited, you should see a header', () => {
@@ -90,48 +130,45 @@ describe('Add New Review View', () => {
       it.only('When the page is visited, you should be able to fill out required info', () => {
         cy.get('.review-form')
         cy.get('.required-container')
-        cy.get('textarea').type('This was an ok date.')
-        cy.get('#star2')
+        cy.get('textarea').type('This was a fine date.')
+        cy.get('#star3')
         .click()
+        cy.get('#dateAgain').select('Yes')
         cy.get('#safetyMeter[type=range]')
         .invoke('val', 10)
         .trigger('change', { data: 10 })
-        // cy.get('#size[type=range]')
-        // .trigger('mousedown')
-        // .trigger('mousemove', 0, 10, { which: 3 })
-        // .trigger('mouseup')
-        // cy.get('#payment[type=range]')
-        // .trigger('mousedown')
-        // .trigger('mousemove', 0, 10, { which: 4 })
-        // .trigger('mouseup')
-        // cy.get('#kindness[type=range]')
-        // .trigger('mousedown')
-        // .trigger('mousemove', 0, 10, { which: 6 })
-        // .trigger('mouseup')
-        // cy.get('#vibe[type=range]')
-        // .trigger('mousedown')
-        // .trigger('mousemove', 0, 10, { which: 6 })
-        // .trigger('mouseup')
-        cy.get('#submitBtn').click({ force: true })
+        cy.get('#gender[type=text]')
+        .invoke('val', "Didn't Ask")
+        .trigger('change', { data: "Didn't Ask" })
+        cy.get('#dateDuration[type=number]')
+        .invoke('val', 2)
+        .trigger('change', { data: 2 })
+        cy.get('#size[type=range]')
+        .invoke('val', 2)
+        .trigger('change', { data: 2 })
+        cy.get('#payment[type=range]')
+        .invoke('val', 2)
+        .trigger('change', { data: 2 })
+        cy.get('#vibe[type=range]')
+        .invoke('val', 2)
+        .trigger('change', { data: 2 })
+        cy.get('#hygiene[type=range]')
+        .invoke('val', 2)
+        .trigger('change', { data: 2 })
+        cy.get('#condomUsage[type=text]')
+        .invoke('val', "Was very willing to use a condom.")
+        .trigger('change', { data: "Was very willing to use a condom." })
+        cy.get('#punctuality[type=text]')
+        .invoke('val', "Showed up a little early, but waited.")
+        .trigger('change', { data: "Showed up a little early, but waited." })
+        cy.request({
+          url:  'https://date-em-rate-em-be.herokuapp.com/graphql',
+          method: 'POST',
+          body: {newClientReviewMutation },
+        })
+        cy.get('#submitBtn').click()
       });
 
-      // .trigger('mousedown', { which: 1 }, { force: true })
-      // .trigger('mousemove', 881, 288, { force: true })
-      // .trigger('mouseup')
-
-
-    // it('When the page is visited, you should be able to fill out the form', () => {
-    //     // cy.get('input').contains('body')
-    //     // cy.get('input[type='submit']')
-    //   });
-
-    //   it('When the page is visited, you should see a header', () => {
-    //     cy.get('img').should("have.class", "nav-logo")
-    //   });
-
-    //   it('When the page is visited, you should see a header', () => {
-    //     cy.get('img').should("have.class", "nav-logo")
-    //   });
 
    
   });
