929860bb1a16fe0f96bb2437329c05aa5432a75c
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-04 21:24:45 -0400

    Add conditionals to displaying data

--- src/Components/ReviewCard.js
+++ src/Components/ReviewCard.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { starRating, formatPayment, formatSize } from '../utils/util';
+import { starRating, formatPayment, formatSize, formatDate } from '../utils/util';
 
 // const ReviewCard = () => {
     const ReviewCard = ({id, key, date, body, title, rating, size, payment, kindness, vibe, gender, dateAgain, safetyMeter, createdAt }) => {
 
+
+
     return (
         <div className="each-review" id={id}>
-        <h2 className="review-title">Client's Review from the date on {createdAt}:</h2>
-            <p className="body">"{body}"</p>
+        <h2 className="review-title">Client's Review from the date on {formatDate(createdAt)}:</h2>
+            {body && <p className="body">"{body}"</p>}
             <p className="safety-meter">How Safe Did I Feel: {safetyMeter}</p>   
-            <p className="rating">Rating: {starRating(rating)} </p>       
-            <p className="size-data">Size: {formatSize(size)}</p>   
-            <p className="payment-data">How Well Did They Pay: {formatPayment(payment)}</p>            
-            <p className="kindness-meter">Kindness: {kindness}/10</p>            
-            <p className="vibe-data">Vibe: {vibe}/10</p>   
-            <p className="date-again">Would I Date Them Again? {dateAgain}</p> 
+            <p className="rating">{starRating(rating)} </p>       
+            {size && <p className="size-data">Size: {formatSize(size)}</p>}
+            {payment && <p className="payment-data">How Well Did They Pay: {formatPayment(payment)}</p>}            
+            {kindness && <p className="kindness-meter">Kindness: {kindness}/10</p>}          
+            {vibe && <p className="vibe-data">Vibe: {vibe}/10</p>}
+            {dateAgain && <p className="date-again">Would I Date Them Again? {dateAgain}</p>}
             {gender && <p className="gender">Gender: {gender}</p>}  
             {/* <p className="date">{date}</p>    */}
             {/* <p className="vibe-data">Hygiene: {hygiene}</p>   
