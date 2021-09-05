da6f186aaa9608c1ec8ba5866f2c0c46cad3b480
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-04 21:24:31 -0400

    Add formatDate func

--- src/utils/util.js
+++ src/utils/util.js
@@ -17,3 +17,11 @@ export const formatSize = (size) => {
         return 'Above average.'
     }
 }
+
+export const formatDate = (time) => {
+    let a, b, date
+    a = time.split('T');
+    b = a[0].split('-');
+    date = b[1] + '/' + b[2] + '/' + b[0];
+    return date
+  }
