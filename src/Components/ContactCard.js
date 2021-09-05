eb891af533593ec3d5654acfffc5104b20160c97
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-05 16:03:44 -0400

    Format client card

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { starRating } from '../utils/util';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { HiOutlineMail } from "react-icons/hi";
 export const ContactCard = ({ id, key, email, averageRating, numReviews }) => {
 
     return (
         <NavLink to={`/profile/${id}`}>
             <article className='contact-card' id={id}>
-                <button className='email'>
-                    <FontAwesomeIcon icon='envelope-square'/>
-                </button>
-                <h3>{email}</h3>
-                <p>Average Rating: {starRating(averageRating)}</p>
+                <div className='email'>
+                    <HiOutlineMail className='email-icon'/>
+                    <h3 className='contact-email'>{email}</h3>
+                </div>
+                <p>average rating: {starRating(averageRating)}</p>
                 <p># of reviews: {numReviews}</p>
                 <button class="view-details-btn">View Client</button>
             </article>
