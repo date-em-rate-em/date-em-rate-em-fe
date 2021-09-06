402db7584916f7d1768010c9b59329d15022000e
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-06 15:25:56 -0600

    Created AboutUs Component to add information about the page.

---
+++ src/Components/AboutUs.js
@@ -0,0 +1,15 @@
+
+
+export const AboutUs = () => {
+
+    return (
+            <article className='about-us-section' id="aboutUs">
+                <section className='about-us-text'>
+                    <p>ClientTell is focused on providing sex-workers a platform to offer feedback about the dates they’ve been on in order to share with their fellow sex-worker communities.</p>
+                    <p>This application allows sex workers to share intricate details about  a given date they’ve been on, rating clients by important, and relevant information that impacts sex-workers.</p>
+                    <p>This will allow other sex-workers to choose whether or not this is a client they would be interested in going out with.</p>
+                    <p>ClienTell intends to stay away from disempowering language that victimizes sex-workers, and instead allows for an empowering, sex-positive, de-stigmatizing  environment.</p>
+                </section>
+            </article>
+    );
+};
