ddf77015408ea7fd06cf895ee57e9bdbc1f910d8
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-09-07 17:34:13 -0400

    Update AboutUs with styling and fix spelling error

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -4,8 +4,9 @@ export const AboutUs = () => {
 
     return (
             <article className='about-us-section' id="aboutUs">
+                <h1 className='about-us-title'>Our Story</h1>
                 <section className='about-us-text'>
-                    <p>ClientTell is focused on providing sex-workers a platform to offer feedback about the dates they’ve been on in order to share with their fellow sex-worker communities.
+                    <p>ClienTell is focused on providing sex-workers a platform to offer feedback about the dates they’ve been on in order to share with their fellow sex-worker communities.
                     This application allows sex workers to share intricate details about  a given date they’ve been on, rating clients by important, and relevant information that impacts sex-workers.
                     This will allow other sex-workers to choose whether or not this is a client they would be interested in going out with.
                     ClienTell intends to stay away from disempowering language that victimizes sex-workers, and instead allows for an empowering, sex-positive, de-stigmatizing  environment.</p>
