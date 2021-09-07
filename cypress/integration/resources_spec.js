0dc456bdc5361a12ac97e72ee54752c66830c97d
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-07 12:12:13 -0600

    Create tests for resources page view.

---
+++ cypress/integration/resources_spec.js
@@ -0,0 +1,39 @@
+import { aliasClientsQuery } from "../utils/graphql-test-utils";
+
+describe('Resources View', () => {
+    beforeEach(() => {
+      cy.intercept(
+        'POST',
+        'https://date-em-rate-em-be.herokuapp.com/graphql',
+        (req) => {
+            //Queries
+          aliasClientsQuery(req, 'Clients');
+        });
+  
+      cy.visit('http://localhost:3000/');
+         cy.get('button').contains('Resources')
+         .click()
+         cy.get('.resources-sections')
+         .location("pathname")
+         .should("eq", "/resources")
+    });
+  
+    it('When the page is visited, you should see a header', () => {
+        cy.get('img').should("have.class", "logo")
+      });
+
+    it('When the page is visited, you should see a live search bar', () => {
+        cy.get('.resources-search-bar')
+      });
+
+      it('When the page is visited, you should see a container of resources', () => {
+        cy.get('.search-result-container')
+      });
+
+    it('When the page is visited, you should be able to search for a resource in the bar', () => {
+        cy.get('.resources-search-bar')
+        .type("molly")
+        cy.get('button').contains('Molly House Project')    
+      });
+
+  });
