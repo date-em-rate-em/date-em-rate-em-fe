15688125b1aca0de29bef22ece4412f5972f51ba
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-07 12:11:08 -0600

    Create tests for about us view.

---
+++ cypress/integration/aboutUs_spec.js
@@ -0,0 +1,31 @@
+import { aliasClientsQuery } from "../utils/graphql-test-utils";
+
+describe('About Us View', () => {
+    beforeEach(() => {
+      cy.intercept(
+        'POST',
+        'https://date-em-rate-em-be.herokuapp.com/graphql',
+        (req) => {
+            //Queries
+          aliasClientsQuery(req, 'Clients');
+        });
+  
+      cy.visit('http://localhost:3000/');
+         cy.get('button').contains('About Us')
+         .click()
+         cy.get('.about-us-section')
+         .location("pathname")
+         .should("eq", "/about-us")
+    });
+  
+    it('When the page is visited, you should see a header', () => {
+        cy.get('img').should("have.class", "logo")
+      });
+
+
+      it('When the page is visited, you should see a section with text describing the application', () => {
+        cy.get('.about-us-section')
+        cy.get('about-us-text').contains('ClientTell is focused on providing sex-workers a platform to offer feedback about the dates they’ve been on in order to share with their fellow sex-worker communities.')
+      });
+
+  });
