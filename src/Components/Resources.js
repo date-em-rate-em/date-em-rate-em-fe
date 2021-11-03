419d87eb8d1b0aaee96cc942b5673f5af2e8a2e5
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-11-03 15:54:51 -0400

    Update resources

--- src/Components/Resources.js
+++ src/Components/Resources.js
@@ -9,11 +9,11 @@ export const Resources = () => {
         {name: 'Urban Justice Center', link: 'https://swp.urbanjustice.org/news-room/'},
         {name: 'Bad Girls Bible', link: 'https://badgirlsbible.com/'},
         {name: 'Sex Workers Outreach Project USA', link: 'https://swopusa.org/'},
-        {name: 'Molly House Project', link: 'https://www.mollyhouseproject.org/'},
+        {name: 'Molly House Project', link: 'https://mollyhouseproject.mystrikingly.com/'},
         {name: 'Red Umbrella Fund', link: 'https://www.redumbrellafund.org/sex-workers-rights/'},
         {name: 'Resourcing Health & Education (RhED)', link: 'https://sexworker.org.au/sex-workers/'},
         {name: 'SAFE in Collingwood ', link: 'https://www.nswp.org/resources'},
-        {name: 'Desiree Alliance', link: 'www.desireealliance.org'},
+        {name: 'Desiree Alliance', link: 'http://desireealliance.org/'},
         {name: 'St. James Infirmary', link: 'https://www.stjamesinfirmary.org/wordpress/'}
 
         ] 
